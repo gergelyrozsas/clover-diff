52053dc3bce129485b3634867c2ceafa5e8a4a12
Gergely Rozsas <rozsasgergo@gmail.com>
2019-10-05 12:59:03 +0200

    Implemented a container that contains project classes.

---
+++ DependencyInjection/ContainerException.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace GergelyRozsas\CloverDiff\DependencyInjection;
+
+use Psr\Container\ContainerExceptionInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class ContainerException extends \RuntimeException implements ContainerExceptionInterface {
+
+}
